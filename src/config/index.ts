80018a76b3c0db6a933f90353641e116bacc20c2
Pradosh <chandpradosh7@gmail.com>
2025-12-28 13:47:12 +0530

    reafctor: db config as separate entity

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,2 @@
-export * from './db';
 export * from './limits';
 export * from './llm';
--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
-import { MONGO_URI } from '../config';
+import { MONGO_URI } from '../config/db';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGO_URI || '');
     console.log('MongoDB connected');
   } catch (error) {
     throw error;
